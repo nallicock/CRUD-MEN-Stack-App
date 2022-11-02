e46e9018151f6ac74b949c61eaea05307acf5006
Nicholas Allicock <nicholasallicock@outlook.com>
2022-11-02 13:57:49 -0400

    adjusted patients ejs view

--- models/doctor.js
+++ models/doctor.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const notesSchema = new Schema({
-  name: String, //patient name
+  patientName: String,
   note: String,
 });
 
