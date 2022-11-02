a6e7cd13c3fcf157b2736854232ba262523d7ac5
Nicholas Allicock <nicholasallicock@outlook.com>
2022-11-02 16:37:46 -0400

    moved notesSchema to patient model

--- models/doctor.js
+++ models/doctor.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const notesSchema = new Schema({
-  patientName: String,
-  note: String,
-});
-
 const doctorSchema = new Schema({
   name: String,
   title: String, //e.g. Surgeon etc
-  notes: [notesSchema],
 });
 
 module.exports = mongoose.model("Doctor", doctorSchema);
--- models/patient.js
+++ models/patient.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const notesSchema = new Schema({
+  patientName: String,
+  note: String,
+});
+
 const patientSchema = new Schema({
   name: String,
   age: Number,
   sickness: String,
   room: Number,
+  notes: [notesSchema],
   doctor: {
     type: Schema.Types.ObjectId,
     ref: "Doctor",
