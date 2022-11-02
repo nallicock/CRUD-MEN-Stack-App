1b2d82dc0457e1252c464f68c8c5e74ab197d814
Nicholas Allicock <nicholasallicock@outlook.com>
2022-11-02 17:58:55 -0400

    connected application to mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/doctors", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.cxysmoc.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
