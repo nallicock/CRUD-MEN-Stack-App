49a850fbb4ecb3476e1bb26c91ad5af636e7dffa
Nicholas Allicock <nicholasallicock@outlook.com>
2022-11-01 23:10:59 -0400

    doctor controller--docList

---
+++ controllers/doctors.js
@@ -0,0 +1,38 @@
+const Doctor = require("../models/doctor");
+const DocNotes = require("../models/docNotes");
+
+function newDoctor(req, res) {
+  res.render("doctor/new");
+}
+
+function docList(req, res) {
+  Doctor.find({}, function (err, doctors) {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.render("doctor/doctors", { doctors });
+    }
+  });
+}
+
+function show(req, res) {
+  Doctor.findById(req.params.id, function (err, doctor) {
+    res.render("doctor/showDoctor", doctor);
+  });
+}
+
+function create(req, res) {
+  console.log(req.body);
+  const doctor = new Doctor(req.body);
+  console.log(doctor);
+
+  doctor.save(function (err, doc) {
+    if (err) {
+      res.send("THERE WAS AN ERROR SAVING!");
+    } else {
+      res.redirect("/doctors");
+    }
+  });
+}
+
+module.exports = { docList, new: newDoctor, create, show };
