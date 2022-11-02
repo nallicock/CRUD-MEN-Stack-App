5664a891424a15742fd09c977174f3bd10e8b4f4
Nicholas Allicock <nicholasallicock@outlook.com>
2022-11-02 16:55:12 -0400

    completed view patient notes ejs

---
+++ controllers/notes.js
@@ -0,0 +1,14 @@
+const Patient = require("../models/patient");
+
+function create(req, res) {
+  Patient.findById(req.params.id, function (err, patient) {
+    patient.notes.push(req.body);
+    patient.save(function (err) {
+      res.redirect(`/patients/${patient._id}`);
+    });
+  });
+}
+
+module.exports = {
+  create,
+};
